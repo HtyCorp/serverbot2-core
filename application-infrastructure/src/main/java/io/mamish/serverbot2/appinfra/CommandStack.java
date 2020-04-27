57b9eafcaa9d20be556f745bff0edbd9fd552c88
Hamish Tym <hamishtym@bigpond.com>
2020-04-28 01:49:30 +1000

    WIP: CodePipeline/CodeBuild setup

--- application-infrastructure/src/main/java/io/mamish/serverbot2/appinfra/CommandStack.java
+++ application-infrastructure/src/main/java/io/mamish/serverbot2/appinfra/CommandStack.java
@@ -3,7 +3,15 @@ package io.mamish.serverbot2.appinfra;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.iam.IManagedPolicy;
+import software.amazon.awscdk.services.iam.ManagedPolicy;
+import software.amazon.awscdk.services.iam.Role;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
+import software.amazon.awscdk.services.lambda.AssetCode;
 import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+
+import java.util.List;
 
 public class CommandStack extends Stack {
     public CommandStack(Construct parent, String id) {
@@ -13,10 +21,22 @@ public class CommandStack extends Stack {
     public CommandStack(Construct parent, String id, StackProps props) {
         super(parent, id, props);
 
+        AssetCode localCodeBuildCode = AssetCode.fromAsset("command-lambda/target/command-lambda.jar");
 
+        List<IManagedPolicy> policyList = List.of(
+                ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"),
+                ManagedPolicy.fromAwsManagedPolicyName("AWSStepFunctionsFullAccess"),
+                ManagedPolicy.fromAwsManagedPolicyName("AmazonSQSFullAccess")
+        );
+        Role functionRole = Role.Builder.create(this, "CommandFunctionExecutionRole")
+                .assumedBy(new ServicePrincipal("lambda.amazonaws.com"))
+                .managedPolicies(policyList)
+                .build();
 
         Function function = Function.Builder.create(this, "CommandFunction")
-                //TODO
+                .code(localCodeBuildCode)
+                .role(functionRole)
+                .runtime(Runtime.JAVA_11)
                 .build();
     }
 }
