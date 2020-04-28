6d36bb4169d2c1c9eed492b4b5010bb2173acdd3
Hamish Tym <hamishtym@bigpond.com>
2020-04-29 00:40:49 +1000

    WIP: ECS infra definition in CDK

---
+++ application-infrastructure/src/main/java/io/mamish/serverbot2/appinfra/RelayStack.java
@@ -0,0 +1,78 @@
+package io.mamish.serverbot2.appinfra;
+
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.ec2.SubnetConfiguration;
+import software.amazon.awscdk.services.ec2.SubnetType;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.iam.ManagedPolicy;
+import software.amazon.awscdk.services.iam.Role;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.RetentionDays;
+
+import java.util.List;
+import java.util.Map;
+
+public class RelayStack extends Stack {
+
+    public RelayStack(Construct parent, String id) {
+        this(parent, id, null);
+    }
+
+    public RelayStack(Construct parent, String id, StackProps props) {
+
+        SubnetConfiguration publicSubnetConfiguration = SubnetConfiguration.builder()
+                .name("PublicSubnet")
+                .subnetType(SubnetType.PUBLIC)
+                .build();
+
+        Vpc vpc = Vpc.Builder.create(this, "DiscordRelayVpc")
+                .maxAzs(1)
+                .natGateways(0)
+                .subnetConfiguration(List.of(publicSubnetConfiguration))
+                .build();
+
+        Cluster cluster = Cluster.Builder.create(this, "DiscordRelayCluster")
+                .vpc(vpc)
+                .build();
+
+        Role taskRole = Role.Builder.create(this, "DiscordRelayRole")
+                .assumedBy(new ServicePrincipal(" ecs-tasks.amazonaws.com"))
+                .managedPolicies(List.of(
+                        ManagedPolicy.fromAwsManagedPolicyName("AmazonSQSFullAccess"),
+                        ManagedPolicy.fromAwsManagedPolicyName("AWSLambdaFullAccess"),
+                        ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess")
+                ))
+                .build();
+
+        TaskDefinition taskDefinition = TaskDefinition.Builder.create(this, "DiscordRelayTask")
+                .compatibility(Compatibility.FARGATE)
+                .cpu("512")
+                .memoryMiB("1024")
+                .taskRole(taskRole)
+                .build();
+
+        LogGroup ecsLogGroup = LogGroup.Builder.create(this, "DiscordRelayLogGroup")
+                .retention(RetentionDays.ONE_YEAR)
+                .build();
+        AwsLogDriverProps logDriverProps = AwsLogDriverProps.builder()
+                .logGroup(ecsLogGroup)
+                .build();
+
+        String dockerDirPath = System.getenv("CODEBUILD_SRC_DIR") + "/relay-docker";
+        ContainerDefinition containerDef = taskDefinition.addContainer("DiscordRelayContainer", ContainerDefinitionOptions.builder()
+                .image(ContainerImage.fromAsset(dockerDirPath))
+                .logging(LogDriver.awsLogs(logDriverProps))
+                .build());
+
+        FargateService service = FargateService.Builder.create(this, "DiscordRelayService")
+                .cluster(cluster)
+                .taskDefinition(taskDefinition)
+                .build();
+
+    }
+
+}
