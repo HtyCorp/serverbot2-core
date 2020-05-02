e3a3c4ef35cacea5be76555561092a0b7c02749d
Ubuntu <ubuntu@ip-172-31-45-71.ap-southeast-2.compute.internal>
2020-05-02 09:17:16 +0000

    WIP: various fixes for Maven and CDK

--- deployment-infrastructure/src/main/java/io/mamish/serverbot2/deployinfra/DeploymentApp.java
+++ deployment-infrastructure/src/main/java/io/mamish/serverbot2/deployinfra/DeploymentApp.java
@@ -3,7 +3,7 @@ package io.mamish.serverbot2.deployinfra;
 import software.amazon.awscdk.core.App;
 
 public class DeploymentApp {
-    public static void main() {
+    public static void main(String[] args) {
         App app = new App();
         new DeploymentStack(app, "DeploymentStack");
         app.synth();
