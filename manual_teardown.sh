6bf2afdce37a6ab05015b55305b8bc9afd7ea792
Hamish Tym <hamishtym@bigpond.com>
2020-05-28 16:51:35 +0000

    WIP: chmod to fix bad teardown script permissons

--- manual_teardown.sh
+++ manual_teardown.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd ~/environment/serverbot2/serverbo2-core
+cd ~/environment/serverbot2/serverbot2-core
 cd application-infrastructure
 echo "Destroying all stacks except CommonResources..."
 cdk destroy AppInstanceResources CommandService DiscordRelay GameMetadataService NetSecService ResourceReaper WorkflowService IpAuthService
