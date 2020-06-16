9eacdfd327b22935fbc7f0ff86f219e468f39020
Hamish Tym <hamishtym@bigpond.com>
2020-06-16 22:21:03 +1000

    Fixed some command help messages

--- command-lambda/src/main/java/io/mamish/serverbot2/commandlambda/commands/admin/CommandDeleteGame.java
+++ command-lambda/src/main/java/io/mamish/serverbot2/commandlambda/commands/admin/CommandDeleteGame.java
@@ -4,7 +4,7 @@ import io.mamish.serverbot2.commandlambda.commands.AbstractCommandDto;
 import io.mamish.serverbot2.framework.common.ApiArgumentInfo;
 import io.mamish.serverbot2.framework.common.ApiRequestInfo;
 
-@ApiRequestInfo(order = 2, name = "DeleteGame", numRequiredFields = 1, description = "Fully delete a game (use with caution!)")
+@ApiRequestInfo(order = 2, name = "deletegame", numRequiredFields = 1, description = "Fully delete a game (use with caution!)")
 public class CommandDeleteGame extends AbstractCommandDto {
 
     @ApiArgumentInfo(order = 0, description = "Name of game to delete")
--- command-lambda/src/main/java/io/mamish/serverbot2/commandlambda/commands/admin/CommandNewGame.java
+++ command-lambda/src/main/java/io/mamish/serverbot2/commandlambda/commands/admin/CommandNewGame.java
@@ -5,7 +5,7 @@ import io.mamish.serverbot2.framework.common.ApiArgumentInfo;
 import io.mamish.serverbot2.framework.common.ApiRequestInfo;
 
 @ApiRequestInfo(order = 1, name = "newgame", numRequiredFields = 1,
-        description = "Create a new blank game server (must install software")
+        description = "Create a new blank game server (must install server software via terminal)")
 public class CommandNewGame extends AbstractCommandDto {
 
     @ApiArgumentInfo(order = 0, description = "Name of new game")
