2c6acb1ea212facd6c831bf2fcb12d1a35ff92aa
Hamish Tym <hamishtym@bigpond.com>
2021-01-30 00:17:14 +1100

    WIP: More prefs editor JS and CSS progress

--- web/deliver-preferences-editor/client.js
+++ web/deliver-preferences-editor/client.js
@@ -1,7 +1,21 @@
+
 function devUnhidePanes() {
     let hiddenElements = document.getElementsByClassName("hide-contents");
     for (let i = 0; i < hiddenElements.length; i++) {
         let pane = hiddenElements.item(i);
         pane.classList.remove("hide-contents");
     }
+}
+
+function authenticatedPost(endpointUrl, authToken, payload) {
+    let payloadWithAuth = { "xWebAuthToken": authToken };
+    Object.assign(payloadWithAuth, payload);
+    let response = fetch(endpointUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-admiralbot-json"
+        },
+        body: JSON.stringify(payloadWithAuth)
+    });
+    return response.json();
 }
